31a54f07437cb02b49df86992bcc64466a5de0f2
Jean Parpaillon <jean.parpaillon@free.fr>
2021-02-05 18:39:20 +0100

    Add range/oid/enum support

--- lib/snmp/mib.ex
+++ lib/snmp/mib.ex
@@ -32,7 +32,7 @@ defmodule Snmp.Mib do
     {:ok, _dest} = compile_mib(src, dest, opts)
     {:ok, mib} = :snmpc_misc.read_mib('#{dest}')
 
-    IO.inspect(mib, label: "MIB")
+    # IO.inspect(mib, label: "MIB")
 
     [
       quote do
@@ -41,30 +41,37 @@ defmodule Snmp.Mib do
         @external_resource unquote(Macro.escape(src))
         @mibname unquote(basename)
 
-        Module.register_attribute(__MODULE__, :varfuns, accumulate: true)
-        Module.register_attribute(__MODULE__, :tablefuns, accumulate: true)
-        Module.register_attribute(__MODULE__, :oids, accumulate: true)
+        Module.register_attribute(__MODULE__, :varfun, accumulate: true)
+        Module.register_attribute(__MODULE__, :tablefun, accumulate: true)
+        Module.register_attribute(__MODULE__, :oid, accumulate: true)
         Module.register_attribute(__MODULE__, :range, accumulate: true)
         Module.register_attribute(__MODULE__, :default, accumulate: true)
-        Module.register_attribute(__MODULE__, :enums, accumulate: true)
+        Module.register_attribute(__MODULE__, :enum, accumulate: true)
 
         @before_compile Snmp.Mib
       end
     ] ++
       Enum.map(mib(mib, :asn1_types), &parse_asn1_type/1) ++
-      Enum.map(mib(mib, :mes), &parse_me/1)
+      Enum.map(mib(mib, :mes), fn me ->
+        IO.inspect(me, label: "ME")
+        parse_me(me)
+      end)
   end
 
   defmacro __before_compile__(env) do
-    for enum <- Module.get_attribute(env.module, :enums) do
+    for enum <- Module.get_attribute(env.module, :enum) do
       gen_enum(enum, env)
     end ++
-      for oid <- Module.get_attribute(env.module, :oids) do
+      for oid <- Module.get_attribute(env.module, :oid) do
         gen_oid(oid)
       end ++
-      for oid <- Module.get_attribute(env.module, :oids) do
+      for oid <- Module.get_attribute(env.module, :oid) do
         gen_oname(oid)
-      end ++ [gen_oids(Module.get_attribute(env.module, :oids), env)]
+      end ++
+      [gen_oids(Module.get_attribute(env.module, :oid), env)] ++
+      for range <- Module.get_attribute(env.module, :range) do
+        gen_range(range)
+      end
   end
 
   defp compile_mib(src, dest, opts) do
@@ -89,19 +96,60 @@ defmodule Snmp.Mib do
 
   defp parse_asn1_type(asn1_type(imported: false, aliasname: name, assocList: [enums: enums])) do
     quote do
-      @enums {unquote(name), unquote(enums)}
+      @enum {unquote(name), unquote(enums)}
     end
   end
 
   defp parse_asn1_type(_), do: []
 
-  defp parse_me(me(oid: oid, aliasname: name)) do
+  defp parse_me(me(oid: oid, asn1_type: :undefined, aliasname: name)) do
     quote do
-      @oids {unquote(oid), unquote(name)}
+      @oid {unquote(oid), unquote(name)}
     end
   end
 
-  defp parse_me(_), do: []
+  defp parse_me(me(oid: oid, asn1_type: type, aliasname: name)) do
+    asn1_type(bertype: bertype, lo: lo, hi: hi) = type
+
+    ast = [
+      quote do
+        @oid {unquote(oid), unquote(name)}
+      end
+    ]
+
+    ast =
+      cond do
+        :undefined == lo or :undefined == hi ->
+          ast
+
+        bertype in [:OCTET_STRING, :Unsigned32, :Counter32, :INTEGER] ->
+          ast ++
+            [
+              quote do
+                @range {unquote(name), unquote(lo), unquote(hi)}
+              end
+            ]
+
+        true ->
+          ast
+      end
+
+    ast =
+      case type do
+        asn1_type(imported: false, assocList: [enums: enums]) ->
+          ast ++
+            [
+              quote do
+                @enum {unquote(name), unquote(enums)}
+              end
+            ]
+
+        _ ->
+          ast
+      end
+
+    ast
+  end
 
   defp gen_enum({name, values}, env) do
     quote do
@@ -133,6 +181,14 @@ defmodule Snmp.Mib do
 
     quote do
       def __oids__, do: unquote(mapping)
+    end
+  end
+
+  defp gen_range({_name, :undefined, :undefined}), do: []
+
+  defp gen_range({name, lo, hi}) do
+    quote do
+      def __range__(unquote(name)), do: {unquote(lo), unquote(hi)}
     end
   end
 end
